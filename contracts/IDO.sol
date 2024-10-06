6d12015505a6cb06da22bb5f7e80a48fa12c8d19
Minh Trong <traminhtrong2604@gmail.com>
2024-10-06 16:57:32 +0700

    fix: design new Project object and modifier for valid project

--- contracts/IDO.sol
+++ contracts/IDO.sol
@@ -5,28 +5,26 @@ contract IDO {
     struct Project {
         address projectOwner;
         address tokenAddress;
-        uint256 tokenSupply;
-        uint256 fund;
+        uint256 tokenForSale;
         uint256 pricePerToken;
         uint256 startTime;
         uint256 endTime;
-        uint256 whiteListCap;
         uint256 raisedAmount;
     }
 
     address public owner;
     mapping(address => mapping(uint256 => uint256)) public balances;
-    mapping(uint256 => mapping(address => bool)) private whitelistedAddresses; // need to fix
+    mapping(uint256 => mapping(address => bool)) private whitelistedAddresses;
     mapping(uint256 => Project) public projects;
     mapping(uint256 => address) public projectOwners;
 
-    uint256 currentProjectID = 0;
+    uint256 currentProjectID = 1;
 
     event Whitelisted(address indexed user, uint256 indexed projectId);
     event ProjectCreated(
+        address indexed projectOwner,
         address indexed tokenAddress,
-        uint256 tokenSupply,
-        uint256 fund,
+        uint256 tokenForSale,
         uint256 pricePerToken,
         uint256 startTime,
         uint256 endTime
@@ -54,12 +52,27 @@ contract IDO {
         );
         _;
     }
+
+    modifier validProject(uint256 _projectId) {
+        if (_projectId > currentProjectID || _projectId <= 0) {
+            revert invalidProjectID();
+        }
+        require(
+            projects[_projectId].tokenAddress != address(0),
+            "Invalid token address"
+        );
+        require(
+            block.timestamp < projects[_projectId].endTime,
+            "Project IDO has ended"
+        );
+        _;
+    }
+
     /**
      * @dev contract errors
      */
     error tokenPriceMustBePositive();
-    error tokenSupplyMustBePositive();
-    error fundMustBePositive();
+    error tokenForSaleMustBePositive();
     error invalidProjectID();
 
     /**
@@ -69,50 +82,36 @@ contract IDO {
      */
     function addProject(
         address _tokenAddress,
-        uint256 _tokenSupply,
-        uint256 _fund,
+        uint256 _tokenForSale,
+        uint256 _pricePerToken,
         uint256 _startTime,
-        uint256 _endTime,
-        uint256 _whiteListCap
+        uint256 _endTime
     ) public {
         require(_tokenAddress != address(0), "Token address need to exist");
-        if (_tokenSupply <= 0) {
-            revert tokenSupplyMustBePositive();
-        }
-        if (_fund <= 0) {
-            revert fundMustBePositive();
+        if (_tokenForSale <= 0) {
+            revert tokenForSaleMustBePositive();
         }
 
         Project memory newProject = Project({
             projectOwner: msg.sender,
             tokenAddress: _tokenAddress,
-            tokenSupply: _tokenSupply,
-            fund: _fund,
-            pricePerToken: 0,
+            tokenForSale: _tokenForSale,
+            pricePerToken: _pricePerToken,
             startTime: _startTime,
             endTime: _endTime,
-            whiteListCap: _whiteListCap,
             raisedAmount: 0
         });
 
         projects[currentProjectID] = newProject;
 
-        uint256 tokenPrice = getProjectTokenPrice(currentProjectID);
-        if (tokenPrice <= 0) {
-            revert tokenPriceMustBePositive();
-        }
-
-        newProject.pricePerToken = tokenPrice;
-
         emit ProjectCreated(
+            msg.sender,
             _tokenAddress,
-            _tokenSupply,
-            _fund,
-            tokenPrice,
+            _tokenForSale,
+            _pricePerToken,
             _startTime,
             _endTime
         );
-        currentProjectID++;
     }
 
     function withdraw(uint256 _projectId) public onlyProjectOwner(_projectId) {
@@ -122,26 +121,16 @@ contract IDO {
         );
         uint256 projectRaisedAmount = projects[_projectId].raisedAmount;
         payable(msg.sender).transfer(projectRaisedAmount);
-        projects[_projectId].fund = 0;
+        projects[_projectId].raisedAmount = 0;
         emit ProjectWithdrawn(msg.sender, _projectId);
     }
 
     /**
      * @dev temp solution before we intergrated Bifrost
      */
-    function investProject(uint256 _projectId) public payable {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-        require(
-            projects[_projectId].tokenAddress != address(0),
-            "Project does not exist"
-        );
-        require(
-            block.timestamp < projects[_projectId].endTime,
-            "Project has ended"
-        );
-
+    function investProject(
+        uint256 _projectId
+    ) public payable validProject(_projectId) {
         projects[_projectId].raisedAmount += msg.value;
         balances[msg.sender][_projectId] += msg.value;
 
@@ -151,44 +140,31 @@ contract IDO {
     /**
      * @dev public function, everyone can sign up for whitelist
      * @dev temp solution until intergrating Bifrost
+     * @dev invest and whitelist must be in a same function
      * @param _projectId the project id
      */
-    function signUpForWhitelist(uint256 _projectId) public {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-        require(
-            projects[_projectId].tokenAddress != address(0),
-            "Project does not exist"
-        );
-        require(
-            !isWhitelisted(_projectId, msg.sender),
-            "Already whitelisted for this project"
-        );
+    // function signUpForWhitelist(
+    //     uint256 _projectId
+    // ) public validProject(_projectId) {
+    //     require(
+    //         projects[_projectId].tokenAddress != address(0),
+    //         "Project does not exist"
+    //     );
+    //     require(
+    //         !isWhitelisted(_projectId, msg.sender),
+    //         "Already whitelisted for this project"
+    //     );
 
-        uint256 investedAmount = getInvestedAmount(msg.sender, _projectId);
-        require(
-            investedAmount >= projects[_projectId].whiteListCap,
-            "Insufficient investment"
-        );
+    //     uint256 investedAmount = getInvestedAmount(msg.sender, _projectId);
+    //     require(
+    //         investedAmount >= projects[_projectId].whiteListCap,
+    //         "Insufficient investment"
+    //     );
 
-        whitelistedAddresses[_projectId][msg.sender] = true;
+    //     whitelistedAddresses[_projectId][msg.sender] = true;
 
-        emit Whitelisted(msg.sender, _projectId);
-    }
-
-    /**
-     * @dev this function calculate listing price of project token
-     * @param _projectId the project id number
-     */
-    function getProjectTokenPrice(
-        uint256 _projectId
-    ) public view returns (uint256) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-        return projects[_projectId].fund / projects[_projectId].tokenSupply;
-    }
+    //     emit Whitelisted(msg.sender, _projectId);
+    // }
 
     /**
      * @dev view functions
@@ -198,52 +174,31 @@ contract IDO {
     function isWhitelisted(
         uint256 _projectId,
         address _address
-    ) public view returns (bool) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
+    ) public view validProject(_projectId) returns (bool) {
         return whitelistedAddresses[_projectId][_address];
     }
 
     function getProjectFullDetails(
         uint256 _projectId
-    ) public view returns (Project memory) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
+    ) public view validProject(_projectId) returns (Project memory) {
         return projects[_projectId];
     }
 
-    function getProjectTokenSupply(
+    function getProjecttokenForSale(
         uint256 _projectId
-    ) public view returns (uint256) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-        return projects[_projectId].tokenSupply;
+    ) public view validProject(_projectId) returns (uint256) {
+        return projects[_projectId].tokenForSale;
     }
 
-    function getProjectFund(uint256 _projectId) public view returns (uint256) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-        return projects[_projectId].fund;
-    }
-
-    function isProjectActive(uint256 _projectId) public view returns (bool) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
-
+    function isProjectActive(
+        uint256 _projectId
+    ) public view validProject(_projectId) returns (bool) {
         return (block.timestamp > projects[_projectId].endTime);
     }
 
     function getTimeLeftUntilProjecctEnd(
         uint256 _projectId
-    ) public view returns (uint256) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
+    ) public view validProject(_projectId) returns (uint256) {
         return projects[_projectId].endTime - block.timestamp;
     }
 
@@ -254,10 +209,7 @@ contract IDO {
     function getInvestedAmount(
         address _userAdr,
         uint256 _projectId
-    ) public view returns (uint256) {
-        if (_projectId > currentProjectID || _projectId < 0) {
-            revert invalidProjectID();
-        }
+    ) public view validProject(_projectId) returns (uint256) {
         uint256 investedAmount = balances[_userAdr][_projectId];
         require(investedAmount > 0, "User  has not invested in this project");
         return investedAmount;
