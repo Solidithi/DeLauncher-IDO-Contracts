b5f429187bce31e001cfcc19b3da976d6185a88d
ducmint864 <nddminh2021@gmail.com>
2024-10-11 15:02:31 +0700

    Commit group: migrate to foundry * Remove hardhat ignition dir

--- ignition/modules/IDO.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("Contract", (m) => {
-    const Contract = m.contract("IDO");
-
-    return { Contract };
-});
